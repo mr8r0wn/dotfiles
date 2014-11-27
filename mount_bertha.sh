e3bdb5f8f420f9e7f6ffdca8e0fd47f46be146b2
mr8r0wn <bruunn@hotmail.com>
2014-11-27 23:44:49 +0100

    NAS mountscripts

---
+++ mount_bertha.sh
@@ -0,0 +1,7 @@
+sudo mount ~/bertha/.andets
+sudo mount ~/bertha/media
+sudo mount ~/bertha/home_bruun
+sudo mount ~/bertha/scan
+sudo mount ~/bertha/games
+sudo mount ~/bertha/torrent
+#sudo mount ~/bertha/www
---
+++ umount_bertha.sh
@@ -0,0 +1,7 @@
+sudo umount ~/bertha/media -l
+sudo umount ~/bertha/.andets -l
+sudo umount ~/bertha/home_bruun -l
+sudo umount ~/bertha/scan -l
+sudo umount ~/bertha/games -l
+sudo umount ~/bertha/torrent -l
+#sudo umount ~/bertha/www -l
